90fa3ba3032bd513c019bcaae63095df64e25ec6
emiliano <emicabanillas.cab@gmail.com>
2022-07-04 22:32:19 -0300

    v4

--- app.js
+++ app.js
@@ -25,12 +25,24 @@ const retirar = (retiro) =>{
             let pw = parseInt(prompt('Ingrese su clave'));
             if (pw == Usuario1.clave) {
                 retiro = parseInt(prompt('Ingrese el monto a retirar'));
-                if(retiro <= Usuario1.dinero){
-                    Usuario1.dinero -= retiro;
-                    alert('Has retirado: $' + retiro);
+                if(retiro <= Usuario1.dinero.Pesos){
+                    Usuario1.dinero.Pesos -= retiro;
+                    contenedor.innerHTML = `
+                    <h2 class="consultasHead">Monto Retirado</h2>
+                    <div class="dineroConsulta">
+                        <div class="dinero montoDepo">
+                        <h3>Retirado:</h3>
+                            <span>$${retiro}<span>
+                        </div>
+                        <div class="dinero">
+                            <h3>Pesos:</h3>
+                            <span>$${Usuario1.dinero.Pesos}<span>
+                        </div>
+                    </div>`
+                    // alert('Has retirado: $' + retiro);
                     let movRetiro = 'Has retirado: $' + retiro;
                     MOVIMIENTOS_CUENTA.push(movRetiro)
-                    alert('Su saldo actual es: $' + Usuario1.dinero);
+                    // alert('Su saldo actual es: $' + Usuario1.dinero.Pesos);
                     consultar();
                     break;
                 }else{
@@ -53,12 +65,14 @@ class CuentaBanco{
     }
 }
 let nombreUser = prompt('Ingrese su Nombre de Usuario');
-nombreUser = nombreUser.toUpperCase();
-let dineroUser = parseInt(prompt('Por favor, sea generoso e ingrese la cantidad de dinero en su cuenta bancaria.'));
-let claveUser = parseInt(prompt('Cree su clave bancaria de 3 digitos.'));
-const Usuario1 = new CuentaBanco(nombreUser, {Pesos: dineroUser, Dolares: 0, Euros: 0, Reales: 0}, claveUser)
+/*nombreUser = nombreUser.toUpperCase();
+let dineroUser = parseInt(prompt('Por favor, ingrese su dinero de su cuenta bancaria.'));
+let claveUser = parseInt(prompt('Cree su clave bancaria de 3 digitos.')); */
+const Usuario1 = new CuentaBanco(nombreUser, {Pesos: 1000, Dolares: 100, Euros: 500, Reales: 460}, 123)
 
 const MONEDAS_EXTRANJERAS = [[{Divisa: "Dolar Venta", Precio: "130,25"},{Divisa: "Dolar Compra", Precio: "124.25"}], [{Divisa: "Euro Venta", Precio: "135,00"}, {Divisa: "Euro Compra", Precio: "128.00"}], [{Divisa: "Real Venta", Precio: "25,80"}, {Divisa: "Real compra", Precio: "21.80"}]]
+
+console.log(MONEDAS_EXTRANJERAS[0][0].Divisa);
 
 let impuestoSolidario = 0.30;
 let impuesto = 0.35;
@@ -72,22 +86,57 @@ const MONEDA_IMPUESTO = MONEDAS_EXTRANJERAS.map(moneda =>{
 
 const MOVIMIENTOS_CUENTA = [];
 
+let contenedor = document.querySelector('.contenedor');
+const bienvenida = document.querySelector('.bienvenida');
+bienvenida.innerHTML = `¡Bienvenido ${Usuario1.nombre}!`
+
+// contenedor = '';
+
+
 const cajeroAutomatico = (retiro) => { 
     if (Usuario1.nombre) {
-        alert('BIENVENIDO ' + Usuario1.nombre + ' AL CAJERO AUTOMATICO');
+        // alert('BIENVENIDO ' + Usuario1.nombre + ' AL CAJERO AUTOMATICO');
         let opciones = prompt('Ingrese su opcion: \n 1. Consulta \n 2. Deposito \n 3. Retiro  \n 4. Prestamos \n 5. Comprar Moneda Extranjera \n 6. Movimientos \n 7. Salir');
         switch (opciones) {
             case '1':
-                alert(`Su saldo actual en pesos es: $ ${Usuario1.dinero.Pesos}.`);
-                alert(`Su saldo actual en dolares es: USD$ ${Usuario1.dinero.Dolares}.`);
-                alert(`Su saldo actual en euros es: € ${Usuario1.dinero.Euros}.`);
-                alert(`Su saldo actual en real es: R$ ${Usuario1.dinero.Reales}.`);
-                consultar();
+                contenedor.innerHTML =
+                `<h2 class="consultasHead">Tu dinero disponible:</h2>
+                <div class="dineroConsulta">
+                    <div class="dinero">
+                        <h3>Pesos:</h3>
+                        <span>$${Usuario1.dinero.Pesos}<span>
+                    </div>
+                    <div class="dinero">
+                        <h3>Dolar:</h3>
+                        <span>USD$${Usuario1.dinero.Dolares}<span>
+                    </div>
+                    <div class="dinero">
+                        <h3>Euros:</h3>
+                        <span>€${Usuario1.dinero.Euros}<span>
+                    </div>
+                    <div class="dinero">
+                        <h3>Real:</h3>
+                        <span>R$${Usuario1.dinero.Reales}<span>
+                    </div>
+                </div>
+                `
+                // consultar();
                 break;
                 case '2':
-                    let deposito = parseInt(prompt('Ingrese el monto a depositar'));
+                    let deposito = parseInt(prompt('Ingrese el monto a depositar en pesos'));
                     Usuario1.dinero.Pesos += deposito;
-                alert('Su saldo actual es: $' + Usuario1.dinero.Pesos);
+                    contenedor.innerHTML = `
+                    <h2 class="consultasHead">Monto Depositado</h2>
+                    <div class="dineroConsulta">
+                        <div class="dinero montoDepo">
+                        <h3>Depositado:</h3>
+                            <span>$${deposito}<span>
+                        </div>
+                        <div class="dinero">
+                            <h3>Pesos:</h3>
+                            <span>$${Usuario1.dinero.Pesos}<span>
+                        </div>
+                    </div>`
                 let movDepo = 'Has depositado: $' + deposito;
                     MOVIMIENTOS_CUENTA.push(movDepo)
                 consultar();
@@ -124,6 +173,29 @@ const cajeroAutomatico = (retiro) => {
                         alert(`Has comprado USD$${monto}.`);
                         Usuario1.dinero.Dolares += monto;
                         Usuario1.dinero.Pesos -= MONEDA_IMPUESTO[0].moneda * dolares;
+                        contenedor.innerHTML = `
+                        <h2 class="consultasHead">Precio de Divisas</h2>
+                        <div>
+                        <div class="divisa">
+                            <h3>Dolar Venta</h3>
+                            <span>$${MONEDAS_EXTRANJERAS[0][0].Precio}<span>
+                        </div>
+                        <div class="divisa">
+                            <h3>Dolar Compra</h3>
+                            <span>$${MONEDAS_EXTRANJERAS[0][1].Precio}<span>
+                        </div>
+                        </div>
+                        <h2 class="consultasHead">Compra de Dolares</h2>
+                        <div class="dineroConsulta">
+                            <div class="dinero montoDepo">
+                            <h3>Dolares</h3>
+                                <span>USD$${Usuario1.dinero.Dolares}<span>
+                            </div>
+                            <div class="dinero">
+                                <h3>Pesos:</h3>
+                                <span>$${Usuario1.dinero.Pesos}<span>
+                            </div>
+                        </div>`
                         let movDivisa = `Has comprado USD$${monto}.`;
                         alert(`Tu saldo actual en pesos es de: $${Usuario1.dinero.Pesos}`)
                         MOVIMIENTOS_CUENTA.push(movDivisa);
